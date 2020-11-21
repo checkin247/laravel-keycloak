c02f4a39c460cf0ede7f1fbf7e567c35dec43cd7
Thomas Liebrand <thomas.liebrand@key-card.com>
2020-11-21 08:44:48 +0100

    authorize with keycloak

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('login', function () {
+    return redirect(config('keycloak.login'));
+})->name('login');
+
 Route::get('/', function () {
     return view('welcome');
 });
